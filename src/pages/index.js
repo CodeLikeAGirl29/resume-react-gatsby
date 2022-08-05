6e5701d684c9f912d20423a93c54eea4a2bf6dca
Lindsey Howard <miss_leading009@yahoo.com>
2022-08-05 18:21:33 -0400

    trying out different gallery/projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,34 @@ import Skills from "../components/skills"
 import Promotion from "../components/Promotion"
 import Footer from "../components/Footer"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Lindseys Portfolio" />
-    <Header></Header>
-    <Work></Work>
-    <About></About>
-    <Skills></Skills>
-    <Promotion></Promotion>
-    <Footer></Footer>
-  </Layout>
-)
+import Helmet from "react-helmet"
+
+import Gallery from "../components/Gallery"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Lindseys Portfolio" />
+      <Header></Header>
+      <section id="two">
+        <h2>Recent Work</h2>
+
+        <Gallery />
+
+        <ul className="actions">
+          <li>
+            <a href="#" className="button">
+              Full Portfolio
+            </a>
+          </li>
+        </ul>
+      </section>
+      <About></About>
+      <Skills></Skills>
+      <Promotion></Promotion>
+      <Footer></Footer>
+    </Layout>
+  )
+}
 
 export default IndexPage
